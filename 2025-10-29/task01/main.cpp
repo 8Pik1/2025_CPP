384bc90cb75b0ed1b76ab5aead306c61b3ad87a3
Nikita <PerevozchikovNikita2007@yandex.ru>
2025-10-29 20:42:36 +0300

    task01

---
+++ 2025-10-29/task01/main.cpp
@@ -0,0 +1,38 @@
+// Дан одномерный массив из целых чисел. Сформировать два новых массива: 
+// один — из положительных элементов исходного массива, другой — из отрицательных.
+// Если элемент равен нулю, не включать его ни в один из новых массивов.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int N = 10;
+    int a[N] = {-9, 16, 14, -44, 875, 0, -1, -8, 68, -72};
+    int b[N];
+    int c[N];
+    int b_count = 0;
+    int c_count = 0;
+    for (int i = 0; i < N; ++i) {
+        if (a[i] > 0){
+            b[b_count] = a[i];
+            ++b_count;
+        }
+        else if (a[i] < 0){
+            c[c_count] = a[i];
+            ++c_count;
+        }
+        cout << a[i] << ' ';
+    }
+    cout << '\n';
+    for (int i = 0; i < b_count; ++i) {
+        cout << b[i] << ' ';
+    }
+    cout << '\n';
+    for (int i = 0; i < c_count; ++i) {
+        cout << c[i] << ' ';
+    }
+}
+
+
+
